4967ec8d09ad47a02b6bd7fc1272d511643c9ffc
Guiller438 <guilleomg26.9@gmail.com>
2023-11-05 19:57:53 -0500

    Se completo el proyecto

--- Proyetct1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Proyetct1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -113,7 +113,7 @@ namespace Proyetct1.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            sif (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
